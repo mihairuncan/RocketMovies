67d855db5aca50d6e004fc2d52e249109c650d79
Valentina Vlad <vlad.valentina.diana@gmail.com>
2020-05-16 15:01:25 +0300

    added Class Movie

---
+++ Server/RocketMoviesAPI/RocketMoviesAPI/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RocketMoviesAPI.Models
+{
+    public class Movie
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string PlotSummary { get; set; }
+        public decimal GrossTakingsAmount { get; set; }
+        public bool IsAvailableOnDVD { get; set; }
+        public Genre GenreId { get; set; }
+    }
+}
