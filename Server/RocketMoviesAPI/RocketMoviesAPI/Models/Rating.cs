ac06359dd287f4472bd080035b60272cc5b50d5a
Alimary28 <alexandru28.pa@gmail.com>
2020-05-16 10:02:51 +0300

    Modified rating enum

--- Server/RocketMoviesAPI/RocketMoviesAPI/Models/Rating.cs
+++ Server/RocketMoviesAPI/RocketMoviesAPI/Models/Rating.cs
@@ -7,10 +7,10 @@ namespace RocketMoviesAPI.Models
 {
     public enum Rating
     {
-        Awful = 1,
-        Bad = 2,
-        Medium = 3,
-        Good = 4,
-        Awesome = 5
+        Awful ,
+        Bad ,
+        Medium ,
+        Good ,
+        Awesome
     }
 }
