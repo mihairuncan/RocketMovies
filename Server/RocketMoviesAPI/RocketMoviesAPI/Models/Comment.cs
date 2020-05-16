4490a13739c6efb69e420173182000cd87b4795f
mihairuncan <runcan.mihai@gmail.com>
2020-05-16 15:19:58 +0300

    created User and Comment entities

---
+++ Server/RocketMoviesAPI/RocketMoviesAPI/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RocketMoviesAPI.Models
+{
+    public class Comment
+    {
+        public long Id { get; set; }
+        public string CommentText { get; set; }
+        public DateTime AddedOn { get; set; }
+    }
+}
