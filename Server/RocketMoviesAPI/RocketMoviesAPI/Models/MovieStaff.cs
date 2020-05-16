93ac13df0525d7cc537401ed131bdfa98578aef4
CristianRad <cristian.mihai.rad@gmail.com>
2020-05-16 15:08:34 +0300

    added MovieStaff entity

---
+++ Server/RocketMoviesAPI/RocketMoviesAPI/Models/MovieStaff.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RocketMoviesAPI.Models
+{
+    public class MovieStaff
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfDeath { get; set; }
+    }
+}
