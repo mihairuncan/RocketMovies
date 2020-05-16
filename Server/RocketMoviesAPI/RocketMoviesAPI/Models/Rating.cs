210c295fbe7194871d5000dae43a03b6631a3f59
Alimary28 <alexandru28.pa@gmail.com>
2020-05-16 09:23:11 +0300

    Task enum modified

---
+++ Server/RocketMoviesAPI/RocketMoviesAPI/Models/Rating.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RocketMoviesAPI.Models
+{
+    public enum Rating
+    {
+        Awful = 1,
+        Bad = 2,
+        Medium = 3,
+        Good = 4,
+        Awesome = 5
+    }
+}
