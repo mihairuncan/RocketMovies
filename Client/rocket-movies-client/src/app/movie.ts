5e27309580fb1b13ff25bdeeeef6034d51528bb0
Felix Negoita <felixnego@gmail.com>
2020-05-18 21:15:13 +0300

    added app movies component, created movie model

---
+++ Client/rocket-movies-client/src/app/movie.ts
@@ -0,0 +1,12 @@
+export interface Movie {
+    id: number;
+    title: string;
+    year: number;
+    plotSummary: string;
+    grossTakingsAmount: number;
+    isAvailableOnDVD: boolean;
+    genre: string;
+    rating: number;
+    pictureURL: string;
+    //TODO comments array
+}
