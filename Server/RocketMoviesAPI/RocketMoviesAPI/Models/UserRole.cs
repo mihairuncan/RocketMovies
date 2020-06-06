9d676bba34af7ca9369cf793ad97f987eeda4b07
Felix Negoita <felixnego@gmail.com>
2020-06-06 17:55:50 +0300

    added user role static class and a role field to the user model

---
+++ Server/RocketMoviesAPI/RocketMoviesAPI/Models/UserRole.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RocketMoviesAPI.Models
+{
+    public static class UserRole
+    {
+        public static string Admin = "Admin";
+        public static string Moderator = "Moderator";
+        public static string User = "User";
+    }
+}
