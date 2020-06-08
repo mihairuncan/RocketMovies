2b206d8c6c74c8045d585da2540a415ec92ef4dd
Mihai <runcan.mihai@gmail.com>
2020-06-08 15:13:18 +0300

    modified update and delete comment API

---
+++ Server/RocketMoviesAPI/RocketMoviesAPI/ViewModels/CommentForUpdate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RocketMoviesAPI.ViewModels
+{
+    public class CommentForUpdate
+    {
+        public long Id { get; set; }
+        public string CommentText { get; set; }
+    }
+}
