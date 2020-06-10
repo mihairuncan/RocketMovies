72c9ec5018c4d01f12aa44f7979176ae027cdfb5
Valentina Vlad <vlad.valentina.diana@gmail.com>
2020-06-10 10:25:54 +0300

    added comments validators class on Server

---
+++ Server/RocketMoviesAPI/RocketMoviesAPI/ModelValidators/CommentForUpdateValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using RocketMoviesAPI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RocketMoviesAPI.ModelValidators
+{
+    public class CommentForUpdateValidator : AbstractValidator<CommentForUpdate>
+    {
+        public CommentForUpdateValidator()
+        {
+            RuleFor(x => x.CommentText).Length(5, 150);
+        }
+    }
+}
